c365deeac4d11849f7aaf5fb2136358c00fadf19
joaoaugustoss <cop.joaoaugusto@gmail.com>
2022-09-19 22:45:34 -0300

    Menu

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,115 +1,133 @@
+/*import 'package:flutter/material.dart';
+import 'LoggedOut.dart';
+
+void main() {
+  runApp(MyApp(
+    home:LoggedOut(),
+    mainMenu:Home(), // MENU > BUCAR RECEITA >
+
+  ));
+}
+// https://www.figma.com/file/kOWQ66hKUXHCjsMDZ5WPXZ/Sprint-1---LDDM
+class MyApp extends StatelessWidget {
+
+}*/
+
 import 'package:flutter/material.dart';
+import 'package:navigation_drawer_menu/navigation_drawer.dart';
+import 'package:navigation_drawer_menu/navigation_drawer_menu.dart';
+import 'package:navigation_drawer_menu/navigation_drawer_menu_frame.dart';
+import 'package:navigation_drawer_menu/navigation_drawer_state.dart';
+
+const likedValueKey = ValueKey('Alarm');
+const ingredientsValueKey = ValueKey('Todo');
+const listsValueKey = ValueKey('Photo');
+
+final Map<Key, MenuItemContent> menuItems = {
+  likedValueKey: MenuItemContent(
+      menuItem: MenuItemDefinition("Liked", likedValueKey,
+          iconData: Icons.favorite)),
+  ingredientsValueKey: MenuItemContent(
+      menuItem: MenuItemDefinition("Ingredients", ingredientsValueKey,
+          iconData: Icons.shopping_basket)),
+  listsValueKey: MenuItemContent(
+      menuItem: MenuItemDefinition("Lists", listsValueKey,
+          iconData: Icons.rule))
+};
+
+const title = 'Resquitem';
+const menuColor = Color(0xFF424242);
+
+final theme = ThemeData(
+    brightness: Brightness.dark,
+    textTheme: const TextTheme(bodyText2: TextStyle(color: Color(0xFFFFFFFF))),
+    primaryColor: Colors.white,
+    backgroundColor: Colors.black);
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _MyAppState extends State<MyApp> {
+  final NavigationDrawerState state = NavigationDrawerState();
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext materialAppContext) => MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: theme,
+      home: Builder(
+          builder: (context) => Scaffold(
+              appBar: AppBar(
+                  title: const Text(title),
+                  leading: Builder(
+                    builder: (iconButtonBuilderContext) => IconButton(
+                      icon: const Icon(Icons.menu),
+                      // TODO abrir telinha pra pesquisa dos filtros
+                      onPressed: () {
+                        state.toggle(iconButtonBuilderContext);
+                        setState(() {});
+                      },
+                      tooltip: 'Menu',
+                    ),
+                  )),
+              drawer: NavigationDrawer(
+                menuBuilder: Builder(builder: getMenu),
+                menuMode: state.menuMode(context),
+              ),
+              body: NavigationDrawerMenuFrame(
+                body: Builder(
+                    builder: (c) => Center(
+                        child: state.selectedMenuKey == null
+                            ? const Text('Not selected')
+                            : Icon(menuItems[state.selectedMenuKey]!
+                            .menuItem!
+                            .iconData))),
+                menuBackgroundColor: menuColor,
+                menuBuilder: Builder(builder: getMenu),
+                menuMode: state.menuMode(context),
+              ))));
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  Widget getMenu(BuildContext context) =>
+      Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        NavigationDrawerMenu(
+            highlightColor: Theme.of(context).indicatorColor,
+            onSelectionChanged: (c, key) {
+              state.selectedMenuKey = key;
+              state.closeDrawer(c);
+              setState(() {});
+            },
+            menuItems: menuItems.values.toList(),
+            selectedMenuKey: state.selectedMenuKey,
+            itemPadding: const EdgeInsets.only(left: 5, right: 5),
+            buildMenuButtonContent: (menuItemDefinition, isSelected,
+                buildContentContext) =>
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Icon(menuItemDefinition.iconData,
+                      color: isSelected
+                          ? Theme.of(buildContentContext).backgroundColor
+                          : Theme.of(buildContentContext)
+                          .textTheme
+                          .bodyText2!
+                          .color),
+                  if (state.menuMode(context) != MenuMode.Thin)
+                    const SizedBox(
+                      width: 10,
+                    ),
+                  if (state.menuMode(context) != MenuMode.Thin)
+                    Text(menuItemDefinition.text,
+                        style: isSelected
+                            ? Theme.of(context).textTheme.bodyText2!.copyWith(
+                            color: Theme.of(buildContentContext)
+                                .backgroundColor)
+                            : Theme.of(buildContentContext).textTheme.bodyText2)
+                ]))
+      ]);
 }
