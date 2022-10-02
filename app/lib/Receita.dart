d333ffa1188b09019e02431638220775d0829af1
Danielle <danielledvieira19@gmail.com>
2022-10-01 21:59:39 -0300

    Ajustes na tela que mostra detalhes da receita.

--- app/lib/Receita.dart
+++ app/lib/Receita.dart
@@ -61,9 +61,10 @@ class _Receita extends State<Receita> {
             ),
             Container(
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    padding: EdgeInsets.only(left: 32),
+                    //padding: EdgeInsets.only(left: 32),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
@@ -73,7 +74,40 @@ class _Receita extends State<Receita> {
                           onPressed: () {},
                         ),
                         Text(widget.receita.quantidadeLikes.toString(),
-
+                            style: TextStyle(
+                              color: Colors.black,
+                            )),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.speaker_notes_outlined,
+                              size: 40, color: Colors.grey),
+                          onPressed: () {},
+                        ),
+                        Text(widget.receita.listaComentarios.length.toString(),
+                            style: TextStyle(
+                              color: Colors.black,
+                            )),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.restaurant_rounded,
+                              size: 40, color: Colors.green),
+                          onPressed: () {},
+                        ),
+                        Text(widget.receita.numeroDePorcoes.toString(),
                             style: TextStyle(
                               color: Colors.black,
                             )),
@@ -82,6 +116,30 @@ class _Receita extends State<Receita> {
                   ),
                 ],
               ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 32, top: 35),
+              child: Row(children: [
+                Text(
+                  "Ingredients:",
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                        letterSpacing: .5,
+                        fontSize: 25,
+                        color: Color.fromARGB(250, 0, 0, 0)),
+                  ),
+                ),
+              ]),
+            ),
+            Container (
+                padding: EdgeInsets.only(left: 32, top: 10),
+              child: Row (
+                children: [
+                  Text("\u2022 ", style: TextStyle(fontSize: 30, color: Colors.black)),
+                  Text("Teste ingrediente", style: TextStyle(fontSize: 20, color: Colors.black)),
+                ],
+              )
             ),
           ],
         ),
