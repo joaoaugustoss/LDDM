6a8e6daea06823288473cd26e9fe8f1725b6d836
joaoaugustoss <cop.joaoaugusto@gmail.com>
2022-11-04 16:02:55 -0300

    objeto comentarios

--- app/lib/Comentario.dart
+++ app/lib/Comentario.dart
@@ -1,17 +1,20 @@
 class Comentario {
   int _id = 0;
   String _comentario = "";
+  String _nomeUsuario = "";
   int _idUsuario = 0;
   int _idReceita = 0;
 
   Comentario(
       int id,
+      String nomeUsuario,
       String comentario,
       int idUsuario,
       int idReceita,
       ) {
     this._id = id;
     this._comentario = comentario;
+    this._nomeUsuario = nomeUsuario;
     this._idUsuario = idUsuario;
     this._idReceita = idReceita;
   }
@@ -22,6 +25,10 @@ class Comentario {
 
   String getComentario() {
     return this._comentario;
+  }
+
+  String getNomeUsuario() {
+    return this._nomeUsuario;
   }
 
   int getIDUsuario() {
@@ -40,6 +47,10 @@ class Comentario {
     this._comentario = comentario;
   }
 
+  void setNomeUsuario(String nomeUsuario) {
+    this._nomeUsuario = nomeUsuario;
+  }
+
   void setIdUsuario(int idUs) {
     this._idUsuario = idUs;
   }
@@ -49,6 +60,6 @@ class Comentario {
   }
 
   String toString() {
-    return "Comentario: id:${_id} - comentario: ${_comentario} - IDUsuario: ${_idUsuario} - IDReceita: ${_idReceita}";
+    return "Comentario: id:$_id - Nome: $_nomeUsuario comentario: $_comentario - IDUsuario: $_idUsuario - IDReceita: $_idReceita";
   }
 }
